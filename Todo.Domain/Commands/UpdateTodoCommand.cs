77a17309be114f2f9d3b876d6d2d23e6035c24f9
guilherme Henrique <guilhermepb00@gmail.com>
2023-05-24 17:33:30 -0300

    Update do projeto do curso

---
+++ Todo.Domain/Commands/UpdateTodoCommand.cs
@@ -0,0 +1,33 @@
+using System;
+using Flunt.Notifications;
+using Flunt.Validations;
+using Todo.Domain.Commands.Contracts;
+
+namespace Todo.Domain.Commands
+{
+    public class UpdateTodoCommand : Notifiable<Notification>, ICommand
+    {
+        public UpdateTodoCommand() { }
+
+        public UpdateTodoCommand(Guid id, string title, string user)
+        {
+            Id = id;
+            Title = title;
+            User = user;
+        }
+
+        public Guid Id { get; set; }
+        public string Title { get; set; }
+        public string User { get; set; }
+
+        public void Validate()
+        {
+            AddNotifications(
+                new Contract<UpdateTodoCommand>()
+                    .Requires()
+                    .IsLowerThan(Title, 3, "Title", "Por favor, descreva melhor esta tarefa!")
+                    .IsLowerThan(User, 6, "User", "Usuário inválido!")
+            );
+        }
+    }
+}
