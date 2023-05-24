a963283c4dab6b8711b3f4d517f09b0ebf0446ad
guilherme Henrique <guilhermepb00@gmail.com>
2023-05-24 19:11:21 -0300

    feat: finish test queries

--- Todo.Domain.Tests/QueryTests/TodoQueriesTests.cs
+++ Todo.Domain.Tests/QueryTests/TodoQueriesTests.cs
@@ -15,18 +15,47 @@ namespace Todo.Domain.Tests.EntityTests
         public TodoQueryTests()
         {
             _items = new List<TodoItem>();
-            _items.Add(new TodoItem("Tarefa 1", "usuarioA", DateTime.Now));
-            _items.Add(new TodoItem("Tarefa 2", "usuarioA", DateTime.Now));
-            _items.Add(new TodoItem("Tarefa 3", "andrebaltieri", DateTime.Now));
-            _items.Add(new TodoItem("Tarefa 4", "usuarioA", DateTime.Now));
-            _items.Add(new TodoItem("Tarefa 5", "andrebaltieri", DateTime.Now));
+            _items.Add(new TodoItem("Tarefa 1", "guilherme", DateTime.Now));
+            _items.Add(new TodoItem("Tarefa 2", "userA", DateTime.Now));
+            _items.Add(new TodoItem("Tarefa 3", "guilherme", DateTime.Now));
+            _items.Add(new TodoItem("Tarefa 4", "userB", DateTime.Now));
+            _items.Add(new TodoItem("Tarefa 5", "UserC", DateTime.Now));
         }
 
         [TestMethod]
-        public void Dada_a_consulta_deve_retornar_tarefas_apenas_do_usuario_andrebaltieri()
+        public void Dada_a_consulta_deve_retornar_tarefas_apenas_do_usuario_guilherme()
         {
-            var result = _items.AsQueryable().Where(TodoQueries.GetAll("andrebaltieri"));
+            var result = _items.AsQueryable().Where(TodoQueries.GetAll("guilherme"));
             Assert.AreEqual(2, result.Count());
         }
+
+        [TestMethod]
+        public void Dada_a_consulta_deve_retornar_tarefas_concluidas_do_usuario_guilherme()
+        {
+            var result = _items.AsQueryable().Where(TodoQueries.GetAllDone("guilherme"));
+            Assert.AreEqual(0, result.Count());
+        }
+
+        [TestMethod]
+        public void Dada_a_consulta_deve_retornar_tarefas_nao_concluidas_do_usuario_guilherme()
+        {
+            var result = _items.AsQueryable().Where(TodoQueries.GetAllUndone("guilherme"));
+            Assert.AreEqual(2, result.Count());
+        }
+
+        [TestMethod]
+        public void Dada_a_consulta_deve_retornar_tarefas_concluidas_de_um_periodo_do_usuario_guilherme()
+        {
+            var result = _items.AsQueryable().Where(TodoQueries.GetByPeriod("guilherme", DateTime.Now, true));
+            Assert.AreEqual(0, result.Count());
+        }
+
+        [TestMethod]
+        public void Dada_a_consulta_deve_retornar_tarefas_nao_concluidas_de_um_periodo_do_usuario_guilherme()
+        {
+            var result = _items.AsQueryable().Where(TodoQueries.GetByPeriod("guilherme", DateTime.Now, false));
+            Assert.AreEqual(2, result.Count());
+        }
+
     }
 }
